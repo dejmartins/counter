87b3b79de992859aaf43610318d85134746b0f5c
Jonathan Martins <85448311+dejmartins@users.noreply.github.com>
2023-03-07 13:51:46 +0100

    counter showing

---
+++ src/components/CounterComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>Counter</h1>
+    <div>{{ count }}</div>
+    <div>
+        <button @click="increment">Increment</button>
+        <button @click="decrement">Decrement</button>
+        <button @click="reset">Reset</button>
+    </div>
+</template>
+
+<script>
+import Counter from '@/composables/counter';
+export default {
+    setup(){
+        const { count, increment, decrement, reset, setValue} = Counter();
+        return { count, increment, decrement, reset, setValue};
+    }
+};
+</script>
