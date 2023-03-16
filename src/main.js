67a8aff2fd94ec52275073eb91325d3eb460d4f4
Jonathan Martins <85448311+dejmartins@users.noreply.github.com>
2023-03-16 19:58:37 +0100

    vue-router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,25 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./assets/main.css";
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "@/view/Home.vue";
+import Error404 from "@/view/ErrorPage.vue";
 
-import './assets/main.css'
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "Home",
+      component: Home,
+    },
+    {
+      path: "*",
+      name: "Error",
+      meta: { title: `Page not found` },
+      component: Error404,
+    },
+  ],
+});
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>About Counter</h1>
+        <p>A counter is a device which stores (and sometimes displays) the number of times a particular event or process has occurred, 
+            often in relationship to a clock.
+             Counters are generally classified as either synchronous or asynchronous. 
+             In synchronous counters, all flip-flops share a common clock and change state at the same time. 
+             In asynchronous counters, each flip-flop has a unique clock, and the flip-flop states change at different times.</p>
+    </div>
+    <div>
+
+    </div>
+</template>
+<script>
+
+</script>
