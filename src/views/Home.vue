3e6efb98486f6527032b053fe560004c76c41f55
Jonathan Martins <85448311+dejmartins@users.noreply.github.com>
2023-03-17 22:05:28 +0100

    vuex

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
     <div>
-        <h1>About Counter</h1>
-        <p>A counter is a device which stores (and sometimes displays) the number of times a particular event or process has occurred, 
-            often in relationship to a clock.
-             Counters are generally classified as either synchronous or asynchronous. 
-             In synchronous counters, all flip-flops share a common clock and change state at the same time. 
-             In asynchronous counters, each flip-flop has a unique clock, and the flip-flop states change at different times.</p>
-             <button>Reset</button>
-    </div>
-    <div>
-        <Button title="Increase" />
-        <Button title="Increase" />
-        <Button title="Increase" />
+
+        <div>
+            <h1>About Counter</h1>
+            <p>A counter is a device which stores (and sometimes displays) the number of times a particular event or process has occurred, 
+                often in relationship to a clock.
+                 Counters are generally classified as either synchronous or asynchronous. 
+                 In synchronous counters, all flip-flops share a common clock and change state at the same time. 
+                 In asynchronous counters, each flip-flop has a unique clock, and the flip-flop states change at different times.</p>
+                 <button>Reset</button>
+        </div>
+        <div>
+            <Button title="Increase" />
+            <Button title="Increase" />
+            <Button title="Increase" />
+        </div>
     </div>
 </template>
 <script>
