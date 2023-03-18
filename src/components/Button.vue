1873b367cab933526d5be32c1d181b10c638da36
Jonathan Martins <85448311+dejmartins@users.noreply.github.com>
2023-03-18 21:35:19 +0100

    bottom layer styling

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -11,3 +11,22 @@ export default {
     props: ["title"]
 }
 </script>
+
+<style scoped>
+button {
+    border-radius: 5px;
+    border: 1.5px solid green;
+    height: 100px;
+    width: 100px;
+    cursor: pointer;
+}
+
+button:active{
+    background-color: rgb(245, 252, 245);
+}
+
+p {
+    text-align: center;
+    font-weight: bold;
+}
+</style>
